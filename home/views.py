f396ab296df80d113bdfe6f51178072722915d33
luan <luan640@gmail.com>
2024-11-06 15:23:22 -0300

    novos campos em programar ordem

--- home/views.py
+++ home/views.py
@@ -127,86 +127,6 @@ def home_predial(request):
     return render(request, 'solicitacoes/solicitacao-predial.html', context)
 
 
-# @login_required
-# def home_predial(request):
-
-#     # Subquery para obter o status da última execução para cada solicitação
-#     subquery = Execucao.objects.filter(
-#         ordem=OuterRef('pk')
-#     ).order_by('-n_execucao').values('n_execucao')[:1]
-
-#     # Consulta das solicitações na área 'producao' com o status da última execução
-#     solicitacoes = Solicitacao.objects.filter(area='predial').annotate(
-#         ultimo_numero_execucao=Coalesce(Subquery(subquery, output_field=IntegerField()), Value(0)),
-#         ultimo_status=Coalesce(Subquery(
-#                 Execucao.objects.filter(ordem=OuterRef('pk')).order_by('-n_execucao').values('status')[:1]
-#             ), Value('em_espera'))
-#     )
-
-#     # Aplicando filtros
-#     solicitante = request.GET.get('solicitante')
-#     setor_id = request.GET.get('setor')
-#     data_abertura = request.GET.get('data_abertura')
-#     status = request.GET.get('ultimo_status')
-
-#     if solicitante:
-#         solicitacoes = solicitacoes.filter(solicitante__nome__icontains=solicitante)
-    
-#     if setor_id:
-#         solicitacoes = solicitacoes.filter(setor_id=setor_id)
-    
-#     if data_abertura:
-#         solicitacoes = solicitacoes.filter(data_abertura=data_abertura)
-
-#     if status:  # Filtro para o status
-#         solicitacoes = solicitacoes.filter(ultimo_status=status)
-
-#     # Paginação
-#     paginator = Paginator(solicitacoes, 10)  # Exibir 10 solicitações por página
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-
-#     # Paginação sem filtro
-#     paginator_not_filter = Paginator(solicitacoes.filter(~Q(ultimo_status='finalizada')), 10)  # Exibir 10 solicitações por página
-#     page_number_not_filter = request.GET.get('page')
-#     page_obj_not_filter = paginator_not_filter.get_page(page_number_not_filter)
-
-#     operadores = operadores_all('predial')
-#     status_choices = list(Execucao.STATUS_CHOICES)
-#     status_choices = [choice for choice in status_choices if choice[0] != 'em_espera']
-#     tipo_manutencao = InfoSolicitacao.TIPO_CHOICES
-
-#     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-#         html = render_to_string('solicitacoes/partials/cards-predial.html', {'page_obj': page_obj,
-#                                                              'status_choices':status_choices,
-#                                                              'tipo_manutencao':tipo_manutencao,
-#                                                              'operadores':operadores})
-        
-#         # Verifique se há uma próxima página
-#         next_page = page_obj.next_page_number() if page_obj.has_next() else None
-
-#         return JsonResponse({
-#             'html': html,
-#             'nextPage': next_page
-#         })
-    
-#     setores = Setor.objects.all()
-
-#     context = {
-#         'page_obj': page_obj_not_filter,
-#         'status_choices':status_choices,
-#         'tipo_manutencao':tipo_manutencao,
-#         'operadores':operadores,
-#         'quantidade_em_aberto': solicitacoes.filter(ultimo_status='em_espera').count(),
-#         'quantidade_finalizada': solicitacoes.filter(ultimo_status='finalizada').count(),
-#         'quantidade_em_execucao': solicitacoes.filter(ultimo_status='em_execucao').count(),
-#         'setores': setores,
-#         'solicitante': solicitante,
-#         'setor_id': setor_id,
-#         'data_abertura': data_abertura,
-#     }
-#     return render(request, 'solicitacoes/solicitacao-predial.html', context)
-
 @login_required
 def home_solicitante(request):
 
@@ -372,7 +292,8 @@ def aguardando_primeiro_atendimento_producao(request):
         'area_manutencao': InfoSolicitacao.AREA_CHOICES,
         'tipo_manutencao': InfoSolicitacao.TIPO_CHOICES,
         'today': timezone.now().date(),  # Data atual sem hora
-        'operadores': Operador.objects.filter(area='producao')
+        'operadores': Operador.objects.filter(area='producao'),
+        'request':request
     }
 
     # Verifica se é uma requisição AJAX
@@ -384,7 +305,8 @@ def aguardando_primeiro_atendimento_producao(request):
             'area_manutencao': InfoSolicitacao.AREA_CHOICES,
             'tipo_manutencao': InfoSolicitacao.TIPO_CHOICES,
             'today': timezone.now().date(),  # Data atual sem hora
-            'operadores': Operador.objects.filter(area='producao')
+            'operadores': Operador.objects.filter(area='producao'),
+            'request':request
         }
 
         html = render_to_string('solicitacoes/partials/cards-producao-aguardando.html', context)
