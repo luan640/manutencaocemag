a1cfad440b92657acd2f901887bf01ce1462ac03
luan <luan640@gmail.com>
2026-01-15 10:15:37 -0300

    atualizando staticfiles

--- staticfiles/js/datatable-list-operador.2f9ad2fcab24.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('#tableOperadores').DataTable({
-      "info": true,             // Informação sobre os registros
-      "autoWidth": false,       // Desativa ajuste automático da largura
-      "responsive": true        // Responsividade
-  });
-});
-
