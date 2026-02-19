ee6a00b4afbbbe4d99dd7f608b4ed491bc9a57df
luan <luan640@gmail.com>
2026-02-19 16:59:12 -0300

    testes

--- manutencao_project/settings/base.py
+++ manutencao_project/settings/base.py
@@ -137,9 +137,15 @@ AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
 
-DEFAULT_FILE_STORAGE = env('DEFAULT_FILE_STORAGE')
+default_storage_backend = env(
+    'DEFAULT_STORAGE_BACKEND',
+    default=env('DEFAULT_FILE_STORAGE', default='storages.backends.s3boto3.S3Boto3Storage')
+)
 
 STORAGES = {
+    "default": {
+        "BACKEND": default_storage_backend,
+    },
     "staticfiles": {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
